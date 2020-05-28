0715acbb73ffb82435d09c853266f99544f0df7f
Nikolas Santis <nikosantis@gmail.com>
2020-05-28 16:55:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+]
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+}
+
+const app = express()
+
+const server = new ApolloServer({
+  introspection: true,
+  playground: true,
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
+
+module.exports = app
