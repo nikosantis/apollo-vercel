284a40ba4b56a1c17ce99e2045312fdfe412543f
Nikolas Santis <nikosantis@gmail.com>
2020-05-28 17:04:24 -0400

    add text in /

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
+app.get('/', (_req, res) => {
+  res.send('Welcome to Graphql Apollo Server in Vercel. Go to /graphql')
+})
+
 app.listen({ port: 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 )
